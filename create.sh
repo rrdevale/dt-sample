45baf5b75eb17259046bd8d542af6e6d6f1f9a4e
Rahul <sredevopsguru@gmail.com>
2021-11-30 09:42:46 +0000

    Daemontools automated installation with simple service conf

---
+++ create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Checking whether service directory available or not"
+if [ -d "/etc/sv/$1/log" ] ; then  echo "Directory already exists" ;  else mkdir -p /etc/sv/$1/log ; fi
+
+cat <<EOF > /root/app.sh
+#!/bin/bash
+echo "This is $1 app"
+EOF
+
+cat <<EOF > /etc/sv/$1/run
+#!/bin/bash
+sleep 10
+echo "starting app"
+exec  /root/app.sh
+EOF
+
+cat <<EOF > /etc/sv/$1/log/run
+#!/bin/bash
+PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin:/bin
+multilog t n100 s16777215 ./main  2>&1
+EOF
+
+chmod 755 /etc/sv/$1/run
+chmod 755 /etc/sv/$1/log/run
+chmod 755 /root/app.sh
+
+echo "Check if symlink exists or not"
+if [ -L "/etc/service/$1" ] ; then echo "Softlink already exists...quitting" ; else ln -s /etc/sv/$1  /etc/service/$1 ; fi
